d5ac65634bae14d6401fc2bcc5891269ebb1fa0e
elisseeff <elisseeff@users.noreply.github.com>
2022-10-24 13:57:20 -0400

    Added propensity example

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
+import pathlib
 from setuptools import setup
 
-setup(name='analyzr-sdk-python',
-      version='1.2.35',
-      description='Python SDK for Analyzr API',
-      author='Analyzr Team',
-      author_email='support@analyzr.ai',
-      license='Apache 2.0',
-      packages=['analyzrclient'],
-      zip_safe=False)
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='analyzr-sdk-python',
+    version='1.2.38',
+    description='Python SDK for Analyzr API',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/analyzr-ai/analyzr-sdk-python",
+    author='Analyzr Team',
+    author_email='support@analyzr.ai',
+    license='Apache 2.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=['analyzrclient'],
+    install_requires=[
+        "scikit-learn",
+    ]
+)

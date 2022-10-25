3ae3104a33eda1c37fa989707bf77276b5dd1ecd
elisseeff <elisseeff@users.noreply.github.com>
2022-10-25 10:57:24 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,11 @@ author = 'Analyzr Team'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'sphinx_rtd_theme',
 ]
 

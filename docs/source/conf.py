9565ac86b7467a5ea80fdaf3534d2aa3227ec00d
elisseeff <elisseeff@users.noreply.github.com>
2022-10-25 15:34:52 -0400

    Current update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,3 +59,6 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 html_static_path = []
+
+#
+autodoc_mock_imports = ['pandas']

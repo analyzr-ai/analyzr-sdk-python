c4d72b2d48cd5cad1eb8155cfb69e05e24a866ae
elisseeff <elisseeff@users.noreply.github.com>
2022-10-25 17:20:54 -0400

    Current update

--- analyzrclient/runner_base.py
+++ analyzrclient/runner_base.py
@@ -1,16 +1,3 @@
-"""
-Copyright (c) 2020-2022 Go2Market Insights, LLC d/b/a Analyzr
-All rights reserved.
-https://analyzr.ai
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions
-of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
 import os, sys, time, json
 import pandas as pd
 from copy import deepcopy

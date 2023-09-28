3d51d3752f9139f3e7c4f65f1d2895f516cb9f7b
elisseeff <elisseeff@users.noreply.github.com>
2023-09-28 11:05:46 -0400

    Current update

--- analyzrclient/constants.py
+++ analyzrclient/constants.py
@@ -18,7 +18,7 @@ from pathlib import Path
 #                          GENERAL PARAMETERS                                  #
 #==============================================================================#
 VERBOSE = True
-CLIENT_VERSION = '1.2.53'
+CLIENT_VERSION = '1.2.60'
 HOME = str(Path.home())
 TEMP_DIR = '{}/.analyzr'.format(HOME)
 

489d82fd06c280d111824eb35897fbe873c775b6
Jake Colleran <jcolleran@g2m.ai>
2023-11-22 09:12:51 -0700

    Current Update v1.3.29

--- analyzrclient/constants.py
+++ analyzrclient/constants.py
@@ -18,7 +18,7 @@ from pathlib import Path
 #                          GENERAL PARAMETERS                                  #
 #==============================================================================#
 VERBOSE = True
-CLIENT_VERSION = '1.3.28'
+CLIENT_VERSION = '1.3.29'
 HOME = str(Path.home())
 TEMP_DIR = '{}/.analyzr'.format(HOME)
 

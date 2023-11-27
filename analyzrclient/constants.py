5d5fce483745077ad4df57f709a4f6767288c657
Jake Colleran <jcolleran@g2m.ai>
2023-11-27 14:37:36 -0700

    Current update - v1.3.43

--- analyzrclient/constants.py
+++ analyzrclient/constants.py
@@ -18,7 +18,7 @@ from pathlib import Path
 #                          GENERAL PARAMETERS                                  #
 #==============================================================================#
 VERBOSE = True
-CLIENT_VERSION = '1.3.42'
+CLIENT_VERSION = '1.3.43'
 HOME = str(Path.home())
 TEMP_DIR = '{}/.analyzr'.format(HOME)
 
--- analyzrclient/runner_cluster.py
+++ analyzrclient/runner_cluster.py
@@ -99,7 +99,8 @@ class ClusterRunner(BaseRunner):
             df3 = merge_cluster_ids(df, pc_id, idx_var)
         else: 
             pc_id = pc_id.reset_index(inplace=True)
-            pc_id = pc_id[[idx_var,'PC_ID']]
+            print(pc_id)
+            pc_id = pc_id['PC_ID']
             df3 = merge_cluster_ids(df, pc_id, idx_var)
         if verbose: print('Merged data with pc_ids...', df3)
         res['data'] = df3
